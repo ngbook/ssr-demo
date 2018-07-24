1ec29eb9ae66816ab44138caacc1d5f4649f0cf1
jsongo <jsongo@qq.com>
2018-07-24 18:34:04 +0800

    first transfer state version

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,13 +1,18 @@
+import { NgModule } from '@angular/core';
+import {
+    BrowserModule,
+    BrowserTransferStateModule
+} from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
     bootstrap: [AppComponent],
 
     imports: [
         BrowserModule.withServerTransition({ appId: 'app-root' }),
+        BrowserTransferStateModule,
 
         AppModule,
 
--- src/app/services/fruits.service.ts
+++ src/app/services/fruits.service.ts
@@ -1,14 +1,36 @@
 
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+import { of } from 'rxjs/observable/of';
+
 import { RequestBase } from './net-base';
+import { PlatformService } from '../platform/platform.service';
+
+const FRUITS_KEY = makeStateKey<Object>('fruits');
 
 @Injectable()
 export class FruitsService extends RequestBase {
+    constructor(
+        http: HttpClient,
+        private platform: PlatformService,
+        private transferState: TransferState
+    ) {
+        super(http);
+    }
 
     fetch(url): Observable<any> {
-        return this.post(url).map((rsp) => {
-            return rsp.body;
-        });
+        const body = this.transferState.get(FRUITS_KEY, null);
+        console.log(body);
+        if (this.platform.isBrowser && body) {
+            return of(body);
+        } else {
+            return this.post(url).map((rsp) => {
+                const _body = rsp.body;
+                this.transferState.set(FRUITS_KEY, _body);
+                return _body;
+            });
+        }
     }
 }
